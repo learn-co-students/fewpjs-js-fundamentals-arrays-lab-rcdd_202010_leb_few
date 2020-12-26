0367bf433cd0f4bcf15346c81e50f25082e129da
Samira Fadi El Samargi <samiraelsamargi2018@gmail.com>
2020-12-26 09:57:58 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your solution here!
+
+
+const append = [ 'Milo' , 'Otis' , 'Garfield' , 'Odie'];
+const prepend = [ 'Odie' , 'Milo' , 'Otis' , 'Garfield'];
+const removeLast = ['Milo' , 'Otis' ];
+const removeFirst = [ 'Otis' , 'Garfield'];
+
+
